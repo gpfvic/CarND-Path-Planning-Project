12da9e8ec57dde8bedbb24dfbce5d3fcb2999064
Pengfei Gao <gpfvic@163.com>
2021-08-19 02:30:14 +0800

    add vehicle object cpp file

---
+++ src/TrajectoryExercise2/vehicle.cpp
@@ -0,0 +1,15 @@
+#include "vehicle.h"
+#include "constants.h"
+#include "helpers.h"
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <math.h>
+#include <iterator>
+#include <random>
+#include <algorithm>
+
+Vehicle::Vehicle(){}
+
+Vehicle::Vehicle(double s,)
